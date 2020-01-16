4ff33c47e07bd59d29704d83bf0cf2e723b744a1
Matt Sullivan <matt.j.sullivan@gmail.com>
2020-01-16 14:39:42 -0800

    Added bottom nav bar with basic styling

--- lib/main.dart
+++ lib/main.dart
@@ -77,8 +77,150 @@ class MyApp extends StatelessWidget {
       builder: (_) => Podcast()..parse(url),
       child: MaterialApp(
         title: 'The Boring Show!',
-        home: EpisodesPage(),
+        home: MyPage(),
       ),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  var navIndex = 0;
+
+  final pages = List<Widget>.unmodifiable([
+    EpisodesPage(),
+    DummyPage(),
+  ]);
+
+  final iconList = List<IconData>.unmodifiable([
+    Icons.hot_tub,
+    Icons.timelapse,
+  ]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[navIndex],
+      bottomNavigationBar: MyNavBar(
+        icons: iconList,
+        onPressed: (i) => setState(() => navIndex = i),
+        activeIndex: navIndex,
+      ),
+    );
+  }
+}
+
+class MyNavBar extends StatefulWidget {
+  const MyNavBar({
+    @required this.icons,
+    @required this.onPressed,
+    @required this.activeIndex,
+  }) : assert(icons != null);
+  final List<IconData> icons;
+  final Function(int) onPressed;
+  final int activeIndex;
+
+  @override
+  _MyNavBarState createState() => _MyNavBarState();
+}
+
+class _MyNavBarState extends State<MyNavBar> with TickerProviderStateMixin {
+  double beaconRadius;
+  double maxBeaconRadius = 20;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    beaconRadius = 0;
+  }
+
+  @override
+  void didUpdateWidget(MyNavBar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('${oldWidget.activeIndex}, ${widget.activeIndex}');
+    if (oldWidget.activeIndex != widget.activeIndex) {
+      _startAnimation();
+    }
+  }
+
+  void _startAnimation() {
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+    final _curve = CurvedAnimation(parent: _controller, curve: Curves.linear);
+    Tween<double>(begin: 0, end: 1).animate(_curve)
+      ..addListener(() {
+        setState(() {
+          beaconRadius = maxBeaconRadius * _curve.value;
+          if (beaconRadius == maxBeaconRadius) {
+            beaconRadius = 0;
+          }
+          print('$beaconRadius, $maxBeaconRadius');
+        });
+      });
+    _controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          for (var i = 0; i < widget.icons.length; i++)
+            CustomPaint(
+                painter: BeaconPainter(
+                  beaconRadius: beaconRadius,
+                  maxBeaconRadius: maxBeaconRadius,
+                ),
+                child: GestureDetector(
+                  child: Icon(widget.icons[i],
+                      color: i == widget.activeIndex
+                          ? Colors.yellow[700]
+                          : Colors.black54),
+                  onTap: () => widget.onPressed(i),
+                ))
+        ],
+      ),
+    );
+  }
+}
+
+class BeaconPainter extends CustomPainter {
+  final double beaconRadius;
+  final double maxBeaconRadius;
+  BeaconPainter({this.beaconRadius, this.maxBeaconRadius});
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (beaconRadius == maxBeaconRadius) {
+      return;
+    }
+    double strokeWidth = beaconRadius < maxBeaconRadius * 0.5
+        ? beaconRadius
+        : maxBeaconRadius - beaconRadius;
+    print('strokeWidth: $strokeWidth');
+    final paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = strokeWidth;
+    canvas.drawCircle(Offset(10, 10), beaconRadius, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class DummyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: const Text('Dummy Page'),
     );
   }
 }
@@ -86,14 +228,14 @@ class MyApp extends StatelessWidget {
 class EpisodesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer<Podcast>(builder: (context, podcast, _) {
+    return Consumer<Podcast>(
+      builder: (context, podcast, _) {
         return podcast.feed != null
             ? EpisodeListView(rssFeed: podcast.feed)
             : Center(
                 child: CircularProgressIndicator(),
               );
-      }),
+      },
     );
   }
 }
