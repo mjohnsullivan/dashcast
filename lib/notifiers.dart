18f04472a1eff57917a3c4e5d2d9911aecc2e12d
Emily Fortuna <efortuna@google.com>
2020-01-17 15:23:37 -0800

    added full offline capabilities

--- lib/notifiers.dart
+++ lib/notifiers.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/widgets.dart';
@@ -5,7 +6,7 @@ import 'package:webfeed/webfeed.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 class PlayStatus with ChangeNotifier {
   bool _isPlaying = false;
@@ -17,6 +18,8 @@ class PlayStatus with ChangeNotifier {
   }
 }
 
+// Emergency offline manual: add variable to Podcast(offline: true) on main.dart line 101
+// also Episode(i, offline: true) on line 143 in main.dart
 class Podcast with ChangeNotifier {
   RssFeed _feed;
   Episode _selectedEpisode;
@@ -48,12 +51,15 @@ class Podcast with ChangeNotifier {
 }
 
 class Episode with ChangeNotifier {
-  Episode(this.item);
+  Episode(this.item, {this.offline = false}) {
+    if (offline) setUpFakeMp3();
+  }
 
   bool _hasNotifiedDownloaded = false;
   RssItem item;
   String _location;
   double _percentDownloaded = 0;
+  bool offline;
 
   double get percentDownloaded => _percentDownloaded;
 
@@ -63,7 +69,36 @@ class Episode with ChangeNotifier {
 
   String get downloadLocation => _location;
 
+  Future setUpFakeMp3() async {
+    // Dumb copy of code from assets to a local file.
+    // Workaround for: https://github.com/flutter/flutter/issues/28162
+    String path = await _getDownloadPath('offline.mp3');
+    var data = await rootBundle.load('assets/surf_shimmy.mp3');
+    var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+    var file = File(path);
+    await file.writeAsBytes(bytes);
+    _location = file.path;
+  }
+
+  Future offlineDownload() async {
+    await setUpFakeMp3();
+    notifyListeners();
+    // Simulate a Download that takes 1.2 seconds.
+    Timer.periodic(Duration(milliseconds: 200), (timer) {
+      _percentDownloaded += .1;
+      notifyListeners();
+      if (_percentDownloaded >= 1) {
+        // To guard against weird floating point errors.
+        _percentDownloaded = 1;
+        timer.cancel();
+      }
+    });
+  }
+
   Future download() async {
+    if (offline) {
+      return offlineDownload();
+    }
     final req = http.Request('GET', Uri.parse(item.guid));
     final res = await req.send();
     if (res.statusCode != 200)
