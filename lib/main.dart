f9ebe078f5bc179aeee0ab57769c41564bd68fd3
Fitz <afitzgibbon@google.com>
2020-05-04 15:37:31 -0700

    Attempt some refactoring... Some of this will need to backtrack

--- lib/main.dart
+++ lib/main.dart
@@ -2,75 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-import 'dart:io';
-
+import 'package:dashcast/player.dart';
+import 'package:dashcast/notifiers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:webfeed/webfeed.dart';
-import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
-
-final url = 'https://itsallwidgets.com/podcast/feed';
-
-final pathSuffix = 'dashcast/downloads';
-
-Future<String> _getDownloadPath(String filename) async {
-  final dir = await getApplicationDocumentsDirectory();
-  final prefix = dir.path;
-  final absolutePath = path.join(prefix, filename);
-  print(absolutePath);
-  return absolutePath;
-}
-
-class Podcast with ChangeNotifier {
-  RssFeed _feed;
-  RssItem _selectedItem;
-  Map<String, bool> downloadStatus;
-
-  RssFeed get feed => _feed;
-  void parse(String url) async {
-    final res = await http.get(url);
-    final xmlStr = res.body;
-    _feed = RssFeed.parse(xmlStr);
-    notifyListeners();
-  }
-
-  RssItem get selectedItem => _selectedItem;
-  set selectedItem(RssItem value) {
-    _selectedItem = value;
-    notifyListeners();
-  }
-
-  void download(RssItem item, [Function(double) callback]) async {
-    final req = http.Request('GET', Uri.parse(item.guid));
-    final res = await req.send();
-    if (res.statusCode != 200)
-      throw Exception('Unexpected HTTP code: ${res.statusCode}');
-
-    final contentLength = res.contentLength;
-    var downloadedLength = 0;
-
-    final file = File(await _getDownloadPath(path.split(item.guid).last));
-    res.stream
-        .map((chunk) {
-          downloadedLength += chunk.length;
-          if (callback != null) callback(downloadedLength / contentLength);
-          return chunk;
-        })
-        .pipe(file.openWrite())
-        .whenComplete(() {
-          print('Downloading complete');
-        })
-        .catchError((e) => print('An Error has occurred!!!: $e'));
-  }
-}
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final url = 'https://itsallwidgets.com/podcast/feed';
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -97,6 +38,7 @@ class _MyPageState extends State<MyPage> {
   ]);
 
   final iconList = List<IconData>.unmodifiable([
+    // TODO why is there a hot tub?
     Icons.hot_tub,
     Icons.timelapse,
   ]);
@@ -205,7 +147,6 @@ class BeaconPainter extends CustomPainter {
     double strokeWidth = beaconRadius < maxBeaconRadius * 0.5
         ? beaconRadius
         : maxBeaconRadius - beaconRadius;
-    print('strokeWidth: $strokeWidth');
     final paint = Paint()
       ..color = Colors.blue
       ..strokeWidth = strokeWidth;
@@ -246,7 +187,7 @@ class EpisodeListView extends StatelessWidget {
     @required this.rssFeed,
   }) : super(key: key);
 
-  final RssFeed rssFeed;
+  final EpisodeFeed rssFeed;
 
   void downloadStatus(double num) => print('$num');
 
@@ -265,7 +206,7 @@ class EpisodeListView extends StatelessWidget {
               trailing: IconButton(
                   icon: Icon(Icons.arrow_downward),
                   onPressed: () {
-                    Provider.of<Podcast>(context).download(i);
+                    i.download();
                     Scaffold.of(context).showSnackBar(
                       SnackBar(
                         content: Text('Downloading ${i.title}'),
@@ -281,148 +222,6 @@ class EpisodeListView extends StatelessWidget {
             ),
           )
           .toList(),
-    );
-  }
-}
-
-class PlayerPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          Provider.of<Podcast>(context).selectedItem.title,
-        ),
-      ),
-      body: SafeArea(child: Player()),
-    );
-  }
-}
-
-class Player extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final podcast = Provider.of<Podcast>(context);
-    return Column(
-      children: [
-        Flexible(
-            flex: 8,
-            child: SingleChildScrollView(
-              child: Column(children: [
-                Image.network(podcast.feed.image.url),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    podcast.selectedItem.description.trim(),
-                  ),
-                ),
-              ]),
-            )),
-        Flexible(
-          flex: 2,
-          child: Material(
-            elevation: 12,
-            child: AudioControls(),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class AudioControls extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        PlaybackButtons(),
-      ],
-    );
-  }
-}
-
-class PlaybackButtons extends StatefulWidget {
-  @override
-  _PlaybackButtonState createState() => _PlaybackButtonState();
-}
-
-class _PlaybackButtonState extends State<PlaybackButtons> {
-  bool _isPlaying = false;
-  FlutterSound _sound;
-  double _playPosition;
-  StreamSubscription<PlayStatus> _playerSubscription;
-
-  @override
-  void initState() {
-    super.initState();
-    _sound = FlutterSound();
-    _playPosition = 0;
-  }
-
-  @override
-  void dispose() {
-    _cleanup();
-    super.dispose();
-  }
-
-  void _cleanup() async {
-    if (_sound.audioState == t_AUDIO_STATE.IS_PLAYING)
-      await _sound.stopPlayer();
-    _playerSubscription.cancel();
-  }
-
-  void _stop() async {
-    await _sound.stopPlayer();
-    setState(() => _isPlaying = false);
-  }
-
-  void _play(String url) async {
-    await _sound.startPlayer(url);
-    _playerSubscription = _sound.onPlayerStateChanged.listen((e) {
-      if (e != null) {
-        print(e.currentPosition);
-        setState(() => _playPosition = (e.currentPosition / e.duration));
-      }
-    });
-    setState(() => _isPlaying = true);
-  }
-
-  void _fastForward() {}
-
-  void _rewind() {}
-
-  @override
-  Widget build(BuildContext context) {
-    final item = Provider.of<Podcast>(context).selectedItem;
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Slider(
-          value: _playPosition,
-          onChanged: null,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(icon: Icon(Icons.fast_rewind), onPressed: null),
-            IconButton(
-              icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
-              onPressed: () {
-                if (_isPlaying) {
-                  _stop();
-                } else {
-                  _play(item.guid);
-                }
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.fast_forward),
-              onPressed: null,
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
